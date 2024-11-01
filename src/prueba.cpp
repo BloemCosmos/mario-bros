a1789b4d708b8a7279c5c7d6c4515b08d46eb20b
BloemCosmos <a23110140@ceti.mx>
2024-11-01 23:26:10 +0000

    modificacion1

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -8,19 +8,37 @@
 using namespace std;
 using namespace ftxui;
 
+int posicionX(int posicion, int velocidad, int tiempo)
+{
+    return posicion + velocidad + tiempo;
+}
+
+int posicionY(int posicion, int velocidad, int tiempo)
+{
+    return 0.5 + (9.81) + (tiempo + tiempo);
+}
+
 int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-    auto can = Canvas(100,100);
-    can.DrawPointCircle(25,25,25);
-    can.DrawPointLine(50,0,0,50);
-    auto ColorSupport = vbox({
-                        });
+    int vix = 1;
+    int viy = 1;
+    int posx = 0;
+    int posy = 0;
+
+    auto can = Canvas(200, 200);
+    auto ColorSupport = vbox({});
+
     while (true)
     {
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-        Element lienzo = bgcolor(Color::Orange1, border(vbox(ColorSupport,canvas(&can))));
+        can.DrawPointCircle(
+            posicionX(posx, vix, frame),
+            posicionY(posy, viy, frame),
+            2);
+
+        Element lienzo = bgcolor(Color::Orange1, border(vbox(ColorSupport, canvas(&can))));
         Render(pantalla, lienzo);
         std::cout << reset_position;
         pantalla.Print();
